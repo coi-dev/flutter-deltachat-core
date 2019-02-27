e454705df14d031bdea44c39a759163e2df6d651
daniel.boehrs <daniel.boehrs@open-xchange.com>
2019-02-27 13:50:45 +0100

    OT-102 talk-5 Re-enable avatar selection in profile view

--- android/tools/buildDcc.sh
+++ android/tools/buildDcc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Triggering Delta Chat Core build via ndk-build (please ensure ndk-build is callable on the path"
+echo "Triggering Delta Chat Core build via ndk-build (please ensure ndk-build is callable from the path)"
 ndk-build
