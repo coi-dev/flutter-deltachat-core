e2078b8fb6ab19a3573e408f011b1cf367add6ca
VP- <898409+VP-@users.noreply.github.com>
2019-10-30 17:15:31 +0100

    Updated to NDK 20

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,3 @@
-APP_PLATFORM := android-14
-APP_ABI := arm64-v8a armeabi-v7a x86
-NDK_TOOLCHAIN_VERSION := 4.9
-APP_STL := gnustl_static
-
+APP_PLATFORM := android-21
+APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
+APP_STL := c++_static
