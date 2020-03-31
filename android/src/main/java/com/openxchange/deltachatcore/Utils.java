c4ec71a73710abc382032a03a8ea724737d4c1c9
Boehrsi <git@boehrsi.de>
2020-03-31 09:03:46 +0200

    Enable file logging #494

--- android/src/main/java/com/openxchange/deltachatcore/Utils.java
+++ android/src/main/java/com/openxchange/deltachatcore/Utils.java
@@ -45,8 +45,19 @@ package com.openxchange.deltachatcore;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+
+import com.b44t.messenger.DcContext;
+import com.openxchange.deltachatcore.handlers.EventChannelHandler;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static android.util.Log.DEBUG;
+import static android.util.Log.ERROR;
+import static android.util.Log.INFO;
+import static android.util.Log.VERBOSE;
+import static android.util.Log.WARN;
 
 public class Utils {
 
@@ -58,6 +69,44 @@ public class Utils {
 
     public static void runOnBackground(final @NonNull Runnable runnable) {
         AsyncTask.THREAD_POOL_EXECUTOR.execute(runnable);
+    }
+
+    public static void logEventAndDelegate(@Nullable EventChannelHandler eventChannelHandler, int logLevel, String tag, String message) {
+        boolean shouldDelegate = eventChannelHandler != null;
+
+        switch (logLevel) {
+            case DEBUG:
+                if (shouldDelegate) {
+                    eventChannelHandler.handleEvent(DcContext.DC_EVENT_INFO, message, logLevel);
+                }
+                Log.d(tag, message);
+                break;
+            case INFO:
+                if (shouldDelegate) {
+                    eventChannelHandler.handleEvent(DcContext.DC_EVENT_INFO, message, logLevel);
+                }
+                Log.i(tag, message);
+                break;
+            case WARN:
+                if (shouldDelegate) {
+                    eventChannelHandler.handleEvent(DcContext.DC_EVENT_WARNING, message, logLevel);
+                }
+                Log.w(tag, message);
+                break;
+            case ERROR:
+                if (shouldDelegate) {
+                    eventChannelHandler.handleEvent(DcContext.DC_EVENT_ERROR, message, logLevel);
+                }
+                Log.e(tag, message);
+                break;
+            case VERBOSE:
+            default:
+                if (shouldDelegate) {
+                    eventChannelHandler.handleEvent(DcContext.DC_EVENT_INFO, message, logLevel);
+                }
+                Log.v(tag, message);
+        }
+
     }
 
 }
